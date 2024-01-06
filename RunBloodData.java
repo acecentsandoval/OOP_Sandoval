73518923ea6187c2ae43c1c337d80d89dc8b4757
Christian Acecent Sandoval <145514332+acecentsandoval@users.noreply.github.com>
2024-01-06 11:26:39 +0800

    Update RunBloodData.java

--- RunBloodData.java
+++ RunBloodData.java
@@ -1,41 +1,43 @@
 import java.util.Scanner;
-class BloodData {
- private static String bloodType;
- private static String rhFactor;
- public BloodData() {
-  bloodType="O";
-  rhFactor="+";
- }
- public BloodData(String bt, String rh) {
-  bloodType = bt;
-  rhFactor = rh;
- }
 
- public void display() {
-  System.out.println(bloodType+rhFactor+" is added to the blood bank."); 
- }
+class BloodData {
+  private String bloodType;
+  private String rhFactor;
+  
+  public BloodData() {
+    bloodType = "O"; 
+    rhFactor = "+";
+  }
+  
+  public BloodData(String bt, String rh) {
+    bloodType = bt;
+    rhFactor = rh;
+  }
+  
+  public void display() {
+    System.out.println(bloodType + rhFactor + " is added to the blood bank."); 
+  }
 }
 
 public class RunBloodData {
 
- public static void main(String[] args) {
-  
-  Scanner sc=new Scanner(System.in); 
-  
-  System.out.print("Enter blood type of patient: ");
-  String input1=sc.nextLine(); 
-  
-  System.out.print("Enter the Rhesus factor (+ or -): ");
-  String input2=sc.nextLine();
-  
-  BloodData bd; 
-  
-  if("".equals(input1) || "".equals(input2)) 
-   bd=new BloodData(); 
-  
-  else    
-   bd=new BloodData(input1,input2); 
-  
-  bd.display(); 
- }
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); 
+    
+    System.out.print("Enter blood type of patient: ");
+    String input1 = sc.nextLine();
+    
+    System.out.print("Enter the Rhesus factor (+ or -): "); 
+    String input2 = sc.nextLine();
+    
+    BloodData bd;
+    
+    if(input1.equals("") || input2.equals("")) {
+      bd = new BloodData(); 
+    } else {
+      bd = new BloodData(input1, input2);
+    }
+    
+    bd.display();
+  }
 }
